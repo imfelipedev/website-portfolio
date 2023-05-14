37cbc958992cd041dc8aa8cc858c68a61399d71e
Felps157 <lipeaugustocastro@gmail.com>
2023-05-13 23:29:35 -0300

    v2

--- src/app/interface/components/Animation.js
+++ src/app/interface/components/Animation.js
@@ -5,7 +5,7 @@ import { useInView } from "react-intersection-observer";
 export function Animation({ children }) {
     const [isInView, setIsInView] = useState(false);
     const { ref, inView } = useInView({
-        threshold: 0.25,
+        threshold: 0.3,
         triggerOnce: true,
     });
 
@@ -16,7 +16,13 @@ export function Animation({ children }) {
     }, [inView]);
 
     return (
-        <motion.div ref={ref} initial={{ opacity: 0, y: 50 }} animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }} transition={{ duration: 1 }}>
+        <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: 80 }}
+            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 80 }}
+            transition={{ duration: 1.5 }}
+            style={{ minHeight: "100px" }} // Define a altura mínima
+        >
             {children}
         </motion.div>
     );
